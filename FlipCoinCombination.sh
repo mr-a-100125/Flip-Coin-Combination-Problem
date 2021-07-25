e998e5fdff21dfedf8d290385fd7e7a365e0ae1d
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 16:01:00 +0530

    Created a triplet and store the results in dictionary.

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -61,3 +61,58 @@ for key in ${!doublet[@]}
 do
 	echo "$key : ${doublet[$key]} %"
 done
+
+declare -A triplet
+hhh=1
+hht=1
+hth=1
+htt=1
+thh=1
+tth=1
+tht=1
+ttt=1
+
+n=100
+while [ $n -gt 0 ]
+do
+	flipCoin1=$((RANDOM%2))
+	flipCoin2=$((RANDOM%2))
+	flipCoin3=$((RANDOM%2))
+	if [ $flipCoin1 -eq 0 ]&&[ $flipCoin2 -eq 0 ]&&[ $flipCoin3 -eq 0 ]
+        then
+                triplet[HHH]=$hhh
+                hhh=$(($hhh+1))
+	elif [ $flipCoin1 -eq 0 ]&&[ $flipCoin2 -eq 0 ]&&[ $flipCoin3 -eq 1 ]
+        then
+                triplet[HHT]=$hht
+                hht=$(($hht+1))
+        elif [ $flipCoin1 -eq 0 ]&&[ $flipCoin2 -eq 1 ]&&[ $flipCoin3 -eq 0 ]
+        then
+                triplet[HTH]=$hth
+                hth=$(($hth+1))
+        elif [ $flipCoin1 -eq 0 ]&&[ $flipCoin2 -eq 1 ]&&[ $flipCoin3 -eq 1 ]
+        then
+                triplet[HTT]=$htt
+                htt=$(($htt+1))
+	elif [ $flipCoin1 -eq 1 ]&&[ $flipCoin2 -eq 0 ]&&[ $flipCoin3 -eq 0 ]
+        then
+                triplet[THH]=$thh
+                thh=$(($thh+1))
+        elif [ $flipCoin1 -eq 1 ]&&[ $flipCoin2 -eq 0 ]&&[ $flipCoin3 -eq 1 ]
+        then
+                triplet[THT]=$tht
+                tht=$(($tht+1))
+        elif [ $flipCoin1 -eq 1 ]&&[ $flipCoin2 -eq 1 ]&&[ $flipCoin3 -eq 0 ]
+        then
+                triplet[TTH]=$tth
+                tth=$(($tth+1))
+        else
+                triplet[TTT]=$ttt
+                ttt=$(($ttt+1))
+	fi
+	n=$(($n-1))
+done
+for key in ${!triplet[@]}
+do
+        echo "$key : ${triplet[$key]} %"
+done
