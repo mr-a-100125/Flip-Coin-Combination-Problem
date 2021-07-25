b0de0c748be97e9a97e2042a4b3311720dd9f27e
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 15:28:30 +0530

    Flipping a coin and storing results in singlet dictionary.

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,27 @@
-#!/bin/bash
+#!/bin/bash 
 echo "Welcome to Flip Coin Combination Problem."
 
-flipCoin=$((RANDOM%2))
-if [ $flipCoin -eq 0 ]
+n=100
+declare -A singlet
+head=1
+tail=1
+while [ $n -gt 0 ]
+do
+	flipCoin=$((RANDOM%2))
+	if [ $flipCoin -eq 0 ]
+	then
+		singlet[Heads]=$head
+		head=$(($head+1))
+	else
+		singlet[Tails]=$tail
+		tail=$(($tail+1))
+	fi
+	n=$(($n-1))
+done
+
+if [ ${singlet[Heads]} -gt ${singlet[Tails]} ]
 then
-	echo "Heads."
+	echo "Heads win with $((${singlet[Heads]}-${singlet[Tails]}))%"
 else
-	echo "Tails." 
+	echo "Tails win with $((${singlet[Tails]}-${singlet[Heads]}))%"
 fi
