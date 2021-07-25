96abafd7aa64a710a90e7d3b99547ab2a048f497
mr-a-100125 <gamidiajay@gmail.com>
2021-07-25 15:39:02 +0530

    Created a doublet and store the results in dictionary.

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -25,3 +25,39 @@ then
 else
 	echo "Tails win with $((${singlet[Tails]}-${singlet[Heads]}))%"
 fi
+
+declare -A doublet
+hh=1
+ht=1
+th=1
+tt=1
+
+n=100
+while [ $n -gt 0 ]
+do
+	flipCoin1=$((RANDOM%2))
+	flipCoin2=$((RANDOM%2))
+	if [ $flipCoin1 -eq 0 ]&&[ $flipCoin2 -eq 0 ]
+	then
+		doublet[HH]=$hh
+		hh=$(($hh+1))
+	elif [ $flipCoin1 -eq 0 ]&&[ $flipCoin2 -eq 1 ]
+	then
+		doublet[HT]=$ht
+		ht=$(($ht+1))
+	elif [ $flipCoin1 -eq 1 ]&&[ $flipCoin2 -eq 0 ]
+	then
+		doublet[TH]=$th
+		th=$(($th+1))
+	else
+		doublet[TT]=$tt 
+		tt=$(($tt+1))
+	fi
+	n=$(($n-1))
+done
+
+
+for key in ${!doublet[@]}
+do
+	echo "$key : ${doublet[$key]} %"
+done
